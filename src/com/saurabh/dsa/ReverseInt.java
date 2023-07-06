65e2c98d235c2442382a1a7f65c01c035bd7812a
SaurabhAdmane <s.admane02@gmail.com>
2023-07-06 20:05:56 +0530

    Initial commit

---
+++ src/com/saurabh/dsa/ReverseInt.java
@@ -0,0 +1,22 @@
+package com.saurabh.dsa;
+
+// * --> 541 = 5 * 100 + 4 * 10 + 1 * 1 = 500 + 40 + 1 = 541
+public class ReverseInt {
+    public static void main(String[] args) {
+        System.out.println("ReverseInt : " + reverse(541));
+    }
+
+    public static int reverse(int inputNum) {
+        int reverseInt = 0;
+        while(inputNum != 0) {
+            int digit = inputNum % 10; // return last digit of number
+
+            if((reverseInt >  Integer.MAX_VALUE/10) || (reverseInt <  Integer.MIN_VALUE/10)) // to check INT min and max length
+                return 0;
+
+            reverseInt = (reverseInt * 10) + digit; // * this is main formula which we already learn in childhood
+            inputNum = inputNum / 10; // remove last digit from number
+        }
+        return reverseInt;
+    }
+}
---
+++ src/com/saurabh/dsa/twoPointerAlgorithm/TwoSum.java
@@ -0,0 +1,8 @@
+package com.saurabh.dsa.twoPointerAlgorithm;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+
+}
