ef74ae3ccd78ab9f2a480e60f0c0e30291f2e4e9
SaurabhAdmane <s.admane02@gmail.com>
2023-07-26 02:50:31 +0530

    - Quick sort

--- src/com/saurabh/dsa/sortingAlgorithm/QuickSort.java
+++ src/com/saurabh/dsa/sortingAlgorithm/QuickSort.java
@@ -1,77 +1,54 @@
 package com.saurabh.dsa.sortingAlgorithm;
 
-public class QuickSort {
 
+public class QuickSort {
 
     public static void main(String[] args) {
 
         int[] inputArray = {6, 3, 9, 5, 2, 0, 8};
 
-        divideArray(inputArray, 0, inputArray.length - 1);
+        quickSort(inputArray, 0, inputArray.length - 1);
+
         for (int i = 0; i < inputArray.length; i++) {
             System.out.println("QuickSort : " + inputArray[i]);
         }
     }
 
-    public static void divideArray(int[] inputArray, int startingIndex, int endingIndex) {
-        if (startingIndex >= endingIndex) {
-            // size is zero then array is divided in single element
-            return;
+    public static void quickSort(int[] inputArray, int l, int h) {
+        if (l < h) {
+            int pivot = partitionLogic(inputArray, l, h);
+            quickSort(inputArray, l, pivot - 1);
+            quickSort(inputArray, pivot + 1, h);
         }
-
-        // to divide array need to find mid point
-        int mid = startingIndex + (endingIndex - startingIndex) / 2;
-
-        divideArray(inputArray, startingIndex, mid); // divide first half array
-
-        divideArray(inputArray, mid + 1, endingIndex); // divide second half array
-
-        sortArray(inputArray, startingIndex, mid, endingIndex);
     }
 
-    public static void sortArray(int[] inputArray, int startingIndex, int midIndex, int endingIndex) {
-        int[] mergedArray = new int[endingIndex - startingIndex + 1]; // original array size
+    // l = low(starting point of array), h= high(ending point of array)
+    public static int partitionLogic(int[] inputArray, int l, int h) {
+        int pivot = inputArray[l];
+        int i = l;
+        int j = h;
+        while (i < j) {
+            while (inputArray[i] <= pivot && i < inputArray.length - 1) {
+                i++;
+            }
 
-        int array1StartIndex = startingIndex; // starting index of array 1
-        int array2StartIndex = midIndex + 1; // starting index of array 2
-        int mergeArrayIndex = 0; // index of merge array
+            while (inputArray[j] > pivot) {
+                j--;
+            }
 
-        while (array1StartIndex <= midIndex && array2StartIndex <= endingIndex) {
-            if (inputArray[array1StartIndex] <= inputArray[array2StartIndex]) {
-                // element of array1 is smaller than element of array2 --> add element of array1 to final merged array
-                mergedArray[mergeArrayIndex] = inputArray[array1StartIndex];
-                mergeArrayIndex++;
-                array1StartIndex++;
-            } else {
-                // element of array2 is smaller than element of array1 --> add element of array2 to final merged array
-                mergedArray[mergeArrayIndex] = inputArray[array2StartIndex];
-                mergeArrayIndex++;
-                array2StartIndex++;
+            if (i < j) {
+                swapPosition(inputArray, i, j);
             }
         }
+        swapPosition(inputArray, j, l);
 
-        /**
-         * below anyone while loop will execute,
-         * if condition of below both while loop is true then above while loop will get execute
-         */
-        // array1 remains element --> add all elements on merged array
-        while (array1StartIndex <= midIndex) {
-            mergedArray[mergeArrayIndex] = inputArray[array1StartIndex];
-            mergeArrayIndex++;
-            array1StartIndex++;
-        }
+        return j;
+    }
 
-        // array2 remains element --> add all elements on merged array
-        while (array2StartIndex <= endingIndex) {
-            mergedArray[mergeArrayIndex] = inputArray[array2StartIndex];
-            mergeArrayIndex++;
-            array2StartIndex++;
-        }
-
-        // copy all element of mergedArray into inputArray(Original Array)
-        for (int i = 0, j = startingIndex; i < mergedArray.length; i++, j++) {
-            inputArray[j] = mergedArray[i];
-        }
+    public static void swapPosition(int[] input, int i1, int i2) {
+        int temp = input[i1];
+        input[i1] = input[i2];
+        input[i2] = temp;
     }
 
 }
